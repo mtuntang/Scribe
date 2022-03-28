2f4a7bdd620846cc638bc7131f9af4edbb2a63ce
Hansel Julio <hanseljulio@yahoo.com>
2022-03-27 21:24:57 -0700

    Added login page and Google authentication

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import reducer,{initialState} from './components/reducer';
+import {StateProvider} from './components/StateProvider';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <React.StrictMode>
+      <StateProvider initialState={initialState} reducer={reducer}>
+        <App />
+      </StateProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
 
 //CHALLENGE:
 //1. Implement the add note functionality.
